f7fea8dae9efb7722db96f22ed5e48bf789dfbac
Ayur <amworkb@gmail.com>
2019-05-15 12:18:56 +0800

    Готово. Сделать прокрутку по нажатию на меню. И кнопку для прокрутки вверх. Сделать адаптивным

--- app/js/script.js
+++ app/js/script.js
@@ -3,3 +3,13 @@ $('.main__nav__element').on('click', function() {
 	$('.main__nav__element__active').removeClass('main__nav__element__active');
 	$(this).addClass('main__nav__element__active');
 });
+
+var map;
+
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 51.811234, lng: 107.598114},
+		zoom: 17
+	});
+}
+    
