3e12d44e5ad99335d5fa9c5ff4b9732970d69004
Ayur <amworkb@gmail.com>
2019-05-16 17:44:34 +0800

    начал делать адаптивность

--- app/js/script.js
+++ app/js/script.js
@@ -1,31 +1,28 @@
 $('.main__nav__element').on('click', function() {
-	console.log(123);
 	$('.main__nav__element__active').removeClass('main__nav__element__active');
 	$(this).addClass('main__nav__element__active');
 });
 
-<<<<<<< HEAD
-
-
-var target = $('.section__about');
-var targetPos = target.offset().top;
-var winHeight = $(window).height();
-var scrollToElem = targetPos - winHeight;
-$(window).scroll(function() {
- 	var winScrollTop = $(this).scrollTop();
- 	
-  	if(winScrollTop > scrollToElem) {
+$(window).scroll(function() {	
+  	if ($(this).scrollTop() > ($('.section__about').offset().top - $(window).height() + 100)) 
     	$('.scrollup__button__wrapper').removeClass('hidden');
-  	}
+
+
+  	if ($(this).scrollTop() <= 100)
+  		$('.scrollup__button__wrapper').addClass('hidden');
 });
-=======
-var map;
+
+$('.scrollup__button__wrapper').on('click', function() {
+	$('html').animate({
+		scrollTop: 0
+	}, 1000);
+});
 
 function initMap() {
+	var map;
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 51.811234, lng: 107.598114},
 		zoom: 17
 	});
 }
     
->>>>>>> f7fea8dae9efb7722db96f22ed5e48bf789dfbac
