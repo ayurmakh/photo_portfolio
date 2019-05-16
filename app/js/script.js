b89ad7fadceaaf0ade50ec5bc1f025c4de749503
Ayur <ayurmakhutoff@gmail.com>
2019-05-16 08:32:16 +0800

    added scroll button

--- app/js/script.js
+++ app/js/script.js
@@ -3,3 +3,17 @@ $('.main__nav__element').on('click', function() {
 	$('.main__nav__element__active').removeClass('main__nav__element__active');
 	$(this).addClass('main__nav__element__active');
 });
+
+
+
+var target = $('.section__about');
+var targetPos = target.offset().top;
+var winHeight = $(window).height();
+var scrollToElem = targetPos - winHeight;
+$(window).scroll(function() {
+ 	var winScrollTop = $(this).scrollTop();
+ 	
+  	if(winScrollTop > scrollToElem) {
+    	$('.scrollup__button__wrapper').removeClass('hidden');
+  	}
+});
